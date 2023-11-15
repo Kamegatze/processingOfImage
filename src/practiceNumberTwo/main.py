61b667a84901ce09571d636d9c91ca77b981cf1d
Alexey Shirayev <aleksi.aleksi2014@yandex.ru>
2023-11-15 16:09:31 +0300

    Редактирование файлов во второй практике

--- src/practiceNumberTwo/main.py
+++ src/practiceNumberTwo/main.py
@@ -2,8 +2,7 @@ import cv2
 import math
 import numpy
 from skimage.io import imread, imsave
-import matplotlib.pyplot as plt
-from skimage.transform import EuclideanTransform, AffineTransform, EssentialMatrixTransform, warp
+from skimage.transform import EuclideanTransform, AffineTransform, warp
 
 def normalize8(image: numpy.ndarray):
   mn = image.min()
@@ -19,24 +18,28 @@ image = imread('./src/practiceNumberTwo/lena.jpg')
 
 # евклидовые преоброзования
 
-euclideanProcessing = EuclideanTransform(rotation=numpy.deg2rad(15), translation=(1, 1))
+euclideanProcessing = EuclideanTransform(rotation=math.radians(15), translation=(1, 1))
 
 image_eulidean = warp(image=image, inverse_map=euclideanProcessing)
 
 imsave('./src/practiceNumberTwo/lena-processing-euclidean.jpg', normalize8(image_eulidean))
 
-affineProcessing = AffineTransform(shear=numpy.deg2rad(15), rotation=numpy.deg2rad(0), translation=(1, 1))
+# аффиновые преоброзования
+
+affineProcessing = AffineTransform(shear=math.radians(15), rotation=math.radians(0), translation=(1, 1))
 
 image_affine = warp(image=image, inverse_map=affineProcessing)
 
 imsave('./src/practiceNumberTwo/lena-processing-affine.jpg', normalize8(image_affine))
 
+# Проективные преоброзования
+
 rows, cols = image.shape
 
 image_essantial = cv2.warpPerspective(image, numpy.float32([
-    [1, 0, 0],
-    [0, 1, 0],
-    [0.0005, 0.00005, 1]
+    [1, 0.1, 0],
+    [0.1, 1, 0],
+    [0.001, 0.00001, 1]
 ]), (rows, cols))
 
 imsave('./src/practiceNumberTwo/lena-processing-essantial.jpg', image_essantial)
