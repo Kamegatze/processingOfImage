fb756b24b8203c782c432db62313af88254717e0
Alexey Shirayev <aleksi.aleksi2014@yandex.ru>
2023-12-17 18:14:35 +0300

    Реализация высокочастотных фильтров

--- src/practiceNumberFour/main.py
+++ src/practiceNumberFour/main.py
@@ -1,53 +1,32 @@
+import cv2
 import numpy
-from skimage.io import imread, imsave
+
+image = cv2.imread('./src/practiceNumberFour/lena.jpg')
+
+# Высокочастотные фильтры в сумме которые даут единицу
+filterHightOne = numpy.array([
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]
+])
 
 
-def line_filter(image: numpy.ndarray, coef: float, 
-                maska: numpy.ndarray = numpy.array([
-                    [1, 1, 1], 
-                    [1, 1, 1], 
-                    [1, 1, 1]
-                    ])) ->  numpy.ndarray:
-    
-    def filter(image: numpy.ndarray, row: int, col: int) -> numpy.uint8:
-        
-        row = numpy.abs(row).astype('int')
-        col = numpy.abs(col).astype('int')
-        
-        return image[row][col]
-        
-    
-    maska = maska.dot(coef)
-    
-    rows, cols = maska.shape
-    
-    diffirenceRow = numpy.round(rows / 2).astype('int')
-    diffirenceCol = numpy.round(cols / 2).astype('int')
-    
-    centerFilterRow:int = numpy.round((rows + diffirenceRow) / 2).astype('int')
-    centerFilterCol:int = numpy.round((cols + diffirenceCol) / 2).astype('int')
-    
-    imageCopy = image.copy()
-    
-    for i in range(len(imageCopy) - 1):
-        for j in range(len(imageCopy[0]) - 1):
-            summa = 0
-            for p in range(rows):
-                for q in range(cols):
-                    summa += filter(
-                        imageCopy, i - centerFilterRow + p, 
-                        j - centerFilterCol + q) * maska[p, q]
-            
-            summa = numpy.abs(summa)
-            
-            if summa <= 255:
-                imageCopy[i, j] = numpy.uint8(numpy.round(summa))
-                
-    return imageCopy
+filterHightTwo = numpy.array([
+    [-1, -1, -1],
+    [-1, 9, -1],
+    [-1, -1, -1]
+])
 
+filterHightThree = numpy.array([
+    [1, -2, 1],
+    [-2, 5, -2],
+    [1, -2, 1]
+])
 
-image = imread('src/practiceNumberFour/lena.jpg')
-imageHightFilter = line_filter(image=image, coef=1, maska=numpy.array([
-	[0, -1, 0],[-1, 5, -1],[0, -1, 0]
-]))
-imsave('src/practiceNumberFour/lena-hight-filter.jpg', imageHightFilter)
+saveImageOne = cv2.filter2D(image, -1, filterHightOne)
+saveImageTwo = cv2.filter2D(image, -1, filterHightTwo)
+saveImageThree = cv2.filter2D(image, -1, filterHightThree)
+
+cv2.imwrite('./src/practiceNumberFour/lena-hight-filter-one.jpg', saveImageOne)
+cv2.imwrite('./src/practiceNumberFour/lena-hight-filter-two.jpg', saveImageTwo)
+cv2.imwrite('./src/practiceNumberFour/lena-hight-filter-three.jpg', saveImageThree)
